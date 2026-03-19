53e59323b5ccc4d06b3f0faac60a8f1ef902bd65
aleatorio101 <lukasdng@gmail.com>
2026-03-19 18:17:04 -0300

    criando repository

---
+++ src/main/java/com/example/repository/ProjetoRepository.java
@@ -0,0 +1,8 @@
+
+package com.example.repository;
+
+import com.example.entity.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+}
